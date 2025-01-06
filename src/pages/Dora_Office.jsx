c617b40a11bd0ce18b370f581ff203e2390a9564
Shruutii06 <shruti050406@gmail.com>
2025-01-06 20:27:39 +0530

    text and img changes

--- src/pages/Dora_Office.jsx
+++ src/pages/Dora_Office.jsx
@@ -2,16 +2,18 @@ import TeamCard from "../components/TeamCard";
 import mainBuilding from "../assets/images/mainBuilding.jpeg";
 import man from "../assets/images/man.png";
 
+
 const Dora_Office = () => {
   return (
     <div className="flex flex-col gap-4 mb-4 font-poppins">
       {/* Hero Section */}
-      <div className="relative">
-        <img
-          className="h-[300px] md:h-[504px] w-full object-cover"
-          src={mainBuilding}
-          alt="Main Building"
-        />
+      <div className="relative overflow-hidden">
+      <img
+  className="w-screen h-[300px] md:h-[504px] object-fill "
+  src={mainBuilding}
+  alt="Main Building"
+/>
+
         <div className="absolute inset-0 bg-[#0B0B0B] opacity-[69%]"></div>
         <div className="flex flex-col absolute inset-0 z-10 items-center justify-center">
           <div className="text-white font-semibold text-lg md:text-xl">WELCOME TO</div>
@@ -23,7 +25,7 @@ const Dora_Office = () => {
 
       {/* DORA Details Section */}
       <div className="flex flex-col my-10 lg:mx-40 mx-6 md:mx-16">
-        <div className="flex flex-col md:flex-row w-full gap-10 md:gap-20 font-poppins ml-10 md:ml-44">
+        <div className="flex flex-col md:flex-row w-full gap-10 md:gap-20 font-poppins  md:ml-44">
           <div className="relative mt-8 md:mt-16 flex justify-center md:block">
             <img
               className="h-[200px] md:h-[320px] w-[160px] md:w-[260px]  " 
@@ -52,8 +54,8 @@ const Dora_Office = () => {
 
         </div>
 
-        <div className="bg-[#E6E6E6] flex flex-col px-20 rounded-sm -translate-y-24 -z-10 pt-28 pb-12">
-            <div className="text-[#003B71] text-2xl md:text-3xl font-bold my-4 md:my-7 text-left ml-28 md:ml-32">
+        <div className="bg-[#E6E6E6] flex flex-col px-20 rounded-sm -translate-y-24 -z-10 pt-28 pb-10">
+            <div className="text-[#003B71] text-2xl md:text-3xl font-bold my-4 md:my-7 text-center md:text-left  md:ml-32 ">
             About DORA
           </div>
           <div className="text-md md:text-lg">
